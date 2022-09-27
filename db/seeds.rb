722fa6fe9e37fc8ddebeaff7dc2cffdc3812af2a
SallyMulupi <mulupisally2gmail.com>
2022-09-27 22:07:10 +0300

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Student.create!(first_name:"Dwayne", last_name:"Johnson", grade:3)
+Student.create!(first_name:"Chris", last_name:"Malcom", grade:8)
+Student.create!(first_name:"Kid", last_name:"Denzel", grade:7)
